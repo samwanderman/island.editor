14e49c48663ea2f98f55f407628347181462a33c
Potapov Sergei <potapov.sa@ueshka.ru>
2016-09-20 17:27:16 +0300

    [+] initial commit

---
+++ src/main/java/ru/swg/island/editor/core/Editor.java
@@ -0,0 +1,22 @@
+/**
+ * @author Potapov Sergei (sam-wanderman@yandex.ru)
+ */
+package ru.swg.island.editor.core;
+
+import java.io.IOException;
+
+import ru.swg.island.common.core.Const;
+import ru.swg.island.common.io.IO;
+import ru.swg.wheelframework.io.Resources;
+import ru.swg.wheelframework.view.Scene;
+
+/**
+ * Launcher
+ */
+public class Editor {
+	public static void main(String[] args) 
+			throws IOException {
+		Resources.init();		
+		new Scene(Resources.getString("app.name"), IO.loadLevel("level1"), Const.WINDOW_WIDTH, Const.WINDOW_HEIGHT);
+	}
+}
