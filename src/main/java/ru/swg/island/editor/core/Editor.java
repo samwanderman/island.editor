44447fd3f7189e441648317d71f2f704b971dacc
Potapov Sergei <potapov.sa@ueshka.ru>
2016-09-27 15:29:12 +0300

    [*] improvement

--- src/main/java/ru/swg/island/editor/core/Editor.java
+++ src/main/java/ru/swg/island/editor/core/Editor.java
@@ -3,19 +3,34 @@
  */
 package ru.swg.island.editor.core;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 
+import ru.swg.island.common.core.object.LandscapeTile;
 import ru.swg.island.common.core.object.Level;
+import ru.swg.island.common.core.object.ObjectTile;
+import ru.swg.island.common.core.object.Tile;
+import ru.swg.island.common.core.object.UnitTile;
+import ru.swg.island.common.io.IO;
 import ru.swg.island.editor.landscape.LandscapeTileBoard;
 import ru.swg.island.editor.unit.UnitTileBoard;
 import ru.swg.wheelframework.event.listener.ObjectListener;
@@ -36,21 +51,38 @@ public final class Editor extends JFrame {
 	
 	private final GameBoard gameBoard;
 	
+	/**
+	 * Starter
+	 * 
+	 * @param args
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static final void main(final String[] args) 
 			throws FileNotFoundException, IOException {
 		Resources.init();
 		new Editor(SCREEN_WIDTH, SCREEN_HEIGHT);
 	}
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param width
+	 * @param height
+	 * @throws IOException
+	 */
 	private Editor(final int width, final int height) 
 			throws IOException {
 		
 		// FIXME - calculate right sizes here
 		gameBoard = new GameBoard(width, height - 40);
     	final Component frameworkAdapter = new FrameworkAdapter(gameBoard, width, height - 40);
+    	setLayout(new BorderLayout());
 		setSize(new Dimension(width, height));
 		setTitle(Resources.getString("title.game.editor"));
-		getContentPane().add(frameworkAdapter);
+		getContentPane().add(frameworkAdapter, BorderLayout.CENTER);
+		getContentPane().add(new JScrollPane(getTilesPanel()), BorderLayout.EAST);
+		getContentPane().add(new JScrollPane(getSettingsPanel()), BorderLayout.WEST);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -68,6 +100,11 @@ public final class Editor extends JFrame {
 		addKeyListener(new KeyAdapter());
 	}
 	
+	/**
+	 * Initialize menu
+	 * 
+	 * @return
+	 */
 	private final JMenuBar getMenu() {
 		final JMenuBar menuBar = new JMenuBar();
 		
@@ -76,14 +113,14 @@ public final class Editor extends JFrame {
 		final JMenuItem newFile = new JMenuItem(Resources.getString("str.new"));
 		newFile.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public final void actionPerformed(final ActionEvent e) {
 				new LevelInfoPanel(new ObjectListener<Level>() {
 					@Override
-					public void on(final Level newLevel) {
+					public final void on(final Level newLevel) {
 						final Level level = newLevel;
 						try {
 							gameBoard.loadLevel(level);
-						} catch (IOException e) {
+						} catch (final IOException e) {
 							Log.error("Failed to load new level");
 						}
 					}
@@ -118,5 +155,72 @@ public final class Editor extends JFrame {
 		menuBar.add(tileMenu);
 		
 		return menuBar;
+	}
+	
+	/**
+	 * Tiles panel
+	 * 
+	 * @return
+	 */
+	private final JTabbedPane getTilesPanel() {
+		final JTabbedPane panel = new JTabbedPane();
+		
+		final JPanel landscapePanel = new JPanel();
+		landscapePanel.setLayout(new BoxLayout(landscapePanel, BoxLayout.Y_AXIS));
+		try {
+			final List<LandscapeTile> landscapeTiles = IO.loadTiles("landscape", LandscapeTile.class);
+			for (final LandscapeTile tile: landscapeTiles) {
+				landscapePanel.add(makeButton(tile));
+			}
+		} catch (final IOException e) { }
+		panel.addTab(Resources.getString("str.landscape_tile"), landscapePanel);
+		
+		final JPanel objectsPanel = new JPanel();
+		objectsPanel.setLayout(new BoxLayout(objectsPanel, BoxLayout.Y_AXIS));
+		try {
+			final List<ObjectTile> objectTiles = IO.loadTiles("objects", ObjectTile.class);
+			for (final ObjectTile tile: objectTiles) {
+				landscapePanel.add(makeButton(tile));
+			}
+		} catch (final IOException e) { }
+		panel.addTab(Resources.getString("str.object_tile"), objectsPanel);
+		
+		final JPanel unitsPanel = new JPanel();
+		unitsPanel.setLayout(new BoxLayout(unitsPanel, BoxLayout.Y_AXIS));
+		try {
+			final List<UnitTile> unitTiles = IO.loadTiles("units", UnitTile.class);
+			for (final UnitTile tile: unitTiles) {
+				unitsPanel.add(makeButton(tile));
+			}
+		} catch (final IOException e) { }
+		panel.addTab(Resources.getString("str.unit_tile"), unitsPanel);
+		
+		return panel;
+	}
+	
+	/**
+	 * Make tile button
+	 * 
+	 * @param tile
+	 * @return
+	 * @throws IOException
+	 */
+	private final <T extends Tile> JButton makeButton(final T tile) 
+			throws IOException {
+		final Image image = Resources.loadImage(tile.getImage());
+		final ImageIcon icon = new ImageIcon(image);
+		final JButton button = new JButton(icon);
+		return button;
+	}
+	
+	/**
+	 * Settings panel
+	 * 
+	 * @return
+	 */
+	private final JPanel getSettingsPanel() {
+		final JPanel panel = new JPanel();
+		panel.add(new JLabel(Resources.getString("str.tile_settings") + ":"));
+		return panel;
 	}
 }
